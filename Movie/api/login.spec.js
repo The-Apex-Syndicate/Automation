7b1f31ffbf50cc7dfc18a909a41863104997cdbc
Vishwa <vissweswaran27@gmail.com>
2025-06-17 14:51:54 +0530

    api testcases

--- Movie/api/login.spec.js
+++ Movie/api/login.spec.js
@@ -1,12 +1,77 @@
-// import { test, expect, request } from '@playwright/test';
-// test('GET users', async () => {
-//   const apiContext = await request.newContext();
+import { test, expect, request } from '@playwright/test';
 
-//   const res = await apiContext.get('https://reqres.in/api/users/2');
+test('Home page', async () => {
+  const apiContext = await request.newContext();
 
-//   expect(res.status()).toBe(200);
+  const res = await apiContext.get('https://pf72lmlw-8080.inc1.devtunnels.ms/home');
 
-//   const body = await res.json();
-//   console.log(body); // Optional: log response
-// });
+  expect(res.status()).toBe(200);
+
+});
+
+
+test('Signup API should return 200 or appropriate status', async () => {
+  const apiContext = await request.newContext();
+
+  const res = await apiContext.post('https://pf72lmlw-8080.inc1.devtunnels.ms/api/signup', {
+    data: {
+      username: "Hai2",
+      email: "hai2@popcorn.com",
+      password: "Visswes@27"
+    }
+  });
+
+  // Assert response status
+  expect(res.status()).toBe(200);
+  
+  // Optional: check the response body
+  const body = await res.json();
+  console.log(body);
+});
+
+
+
+test('Signin API should return 200 or appropriate status', async () => {
+  const apiContext = await request.newContext();
+
+  const res = await apiContext.post('https://pf72lmlw-8080.inc1.devtunnels.ms/api/signin', {
+    data: {
+      email: "hai2@popcorn.com",
+      password: "Visswes@27"
+    }
+  });
+
+  // Assert response status
+  expect(res.status()).toBe(200);
+  
+  // Optional: check the response body
+  const body = await res.json();
+  console.log(body);
+});
+
+
+test('Show Individual Movie should return 200', async () => {
+  const apiContext = await request.newContext();
+
+  const res = await apiContext.get('https://pf72lmlw-8080.inc1.devtunnels.ms/api/movies/76600/');
+
+  expect(res.status()).toBe(200);
+
+  const body = await res.json();
+  console.log(body);
+});
+
+
+
+test('Production Houses Movies', async () => {
+  const apiContext = await request.newContext();
+
+  const res = await apiContext.get('https://pf72lmlw-8080.inc1.devtunnels.ms/api/movies/?production_companies=3&page=1');
+
+  expect(res.status()).toBe(200);
+
+  const body = await res.json();
+  console.log(body); 
+});
+
 
